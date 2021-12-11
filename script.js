86a1d0ef016763a37c54a5c4dcbe6820e952c99a
Matthew Wilkins <MatthewWilkins@matthews-mbp.attlocal.net>
2021-12-11 18:20:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+const toDoInput = document.getElementById("to-do-item");
+const list = document.getElementById('list');
+const clearAllButton = document.getElementById('clear-all-button');
+const submitButton = document.getElementById('submit-button');
+const listContainer = document.getElementsByClassName("list-container")[0];
+const form = document.getElementById("form");
+
+function addToList() {
+    if (toDoInput.value){
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    list.appendChild(li).innerText = toDoInput.value;
+    li.appendChild(deleteButton).setAttribute("class", "delete-button");
+    deleteButton.setAttribute("id", "delete-button");
+    deleteButton.innerText = 'Delete'
+    toDoInput.value = '';
+    } else {
+        alert("Taking the day off?")
+    }
+}
+
+function deleteItem(e){
+    if(e.target.getAttribute("id") == "delete-button"){
+        e.target.parentNode.remove();
+    }
+}
+
+function clearAll(){
+    list.querySelectorAll("li").forEach(node => node.remove());
+}
+
+function handleForm(e){
+    e.preventDefault();
+} 
+
+listContainer.onclick = deleteItem;
+submitButton.onclick = addToList;
+clearAllButton.onclick = clearAll;
+form.onsubmit = handleForm;
+
+toDoInput.onkeyup = function(e) {
+    if (e.key == 'Enter'){
+        addToList();
+    }
+}
+
+
+
+
+
+
+
